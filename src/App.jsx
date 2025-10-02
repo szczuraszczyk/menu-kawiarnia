95b503e245ddc44b0cc865c9e4758d9caddedac4
szczuraszczyk <ameli.szczelaszczyk@gmail.com>
2025-10-02 10:33:33 +0200

    first commit + coffee data table

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+const coffeeData=[
+  {id: 0, photo: "", name: "", type: "", description: "", new: true},
+  {id: 0, photo: "", name: "", type: "", description: "", new: true},
+  {id: 0, photo: "", name: "", type: "", description: "", new: true},
+  {id: 0, photo: "", name: "", type: "", description: "", new: true},
+  {id: 0, photo: "", name: "", type: "", description: "", new: true},
+]
+
+function App() {
+
+  return (
+    <div className="site">
+      <div className="coffee-list">
+        </div>
+    </div>
+  )
+}
+
+export default App
