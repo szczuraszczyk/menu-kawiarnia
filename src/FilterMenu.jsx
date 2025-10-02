667849527acb571e97b8639c90c6314b8af71be7
szczuraszczyk <ameli.szczelaszczyk@gmail.com>
2025-10-02 12:05:25 +0200

    added buttons to filter

---
+++ src/FilterMenu.jsx
@@ -0,0 +1,13 @@
+function FilterMenu(){
+
+    return(
+        <div className="filter-menu">
+            <button>ALL</button>
+            <button>COLD BREW</button>
+            <button>DRIP</button>
+            <button>ESPRESSO</button>
+        </div>
+    )
+}
+
+export default FilterMenu;
