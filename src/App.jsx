906a2bdcd4968af57133aeeb6f6726483856589a
szczuraszczyk <ameli.szczelaszczyk@gmail.com>
2025-10-09 12:06:01 +0200

    added description of coffees

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import './App.css'
 import CoffeeList from './CoffeeList'
 
 const coffeeData=[
-  {id: 0, className: "filterCoffee cold-brew", photo: "./Cold-brew-1.jpg", name: "Cold Darkness", type: "cold brew", description: "", isNew: true},
-  {id: 1, className: "filterCoffee latte", photo: "./Latte-1.jpg", name: "Lovely Foam", type: "latte", description: "", isNew: false},
-  {id: 2, className: "filterCoffee cappuccino", photo: "./Cappuccino-1.jpg", name: "Calm Knoll", type: "cappuccino", description: "", isNew: true},
-  {id: 3, className: "filterCoffee cold-brew", photo: "./Cold-brew-2.jpg", name: "Fiery Dance", type: "cold brew", description: "", isNew: false},
-  {id: 4, className: "filterCoffee latte", photo: "./Latte-2.jpg", name: "Sweet Spiral", type: "latte", description: "", isNew: true},
-  {id: 5, className: "filterCoffee cappuccino", photo: "./Cappuccino-2.png", name: "Lone Pyramid", type: "cappuccino", description: "", isNew: false},
+  {id: 0, className: "filterCoffee cold-brew", photo: "./Cold-brew-1.jpg", name: "Cold Darkness", type: "cold brew", description: "nyan nyan nyan nyan nyan ", isNew: true},
+  {id: 1, className: "filterCoffee latte", photo: "./Latte-1.jpg", name: "Lovely Foam", type: "latte", description: "nyan nyan nyan nyan nyan ", isNew: false},
+  {id: 2, className: "filterCoffee cappuccino", photo: "./Cappuccino-1.jpg", name: "Calm Knoll", type: "cappuccino", description: "nyan nyan nyan nyan nyan ", isNew: true},
+  {id: 3, className: "filterCoffee cold-brew", photo: "./Cold-brew-2.jpg", name: "Fiery Dance", type: "cold brew", description: "nyan nyan nyan nyan nyan ", isNew: false},
+  {id: 4, className: "filterCoffee latte", photo: "./Latte-2.jpg", name: "Sweet Spiral", type: "latte", description: "nyan nyan nyan nyan nyan ", isNew: true},
+  {id: 5, className: "filterCoffee cappuccino", photo: "./Cappuccino-2.png", name: "Lone Pyramid", type: "cappuccino", description: "nyan nyan nyan nyan nyan ", isNew: false},
 ]
 
 function App() {
